302fcd27d67aeb464c021baae40be7c9c2853706
Natthiphalak <nattipa.2547@gmail.com>
2024-12-24 17:16:13 +0700

    t

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,18 +1,15 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-// Function Prototype
 int fibonacci(int x);
 
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    cout << "Fibonacci(" << n << ") = " << fibonacci(n) << endl;
-    return 0;
+int main(){
+    int x;
+    cin >> x;
+    cout << fibonacci(x);
+    return 0; 
 }
 
-// Function Definition
 int fibonacci(int x) {
     if (x == 0) {
         return 0;
